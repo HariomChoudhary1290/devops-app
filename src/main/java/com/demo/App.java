abf764aa91dd16cd67f5988a7120de8de18db153
HariomChoudhary1290 <hariom.choudhary015@gmail.com>
2026-03-21 12:57:56 +0530

    Initial commit: Java Maven Project ready

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
