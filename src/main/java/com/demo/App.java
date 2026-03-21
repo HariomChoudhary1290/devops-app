5a1b820f1adce9f5eeb647ce02d0080eea2451f3
HariomChoudhary1290 <hariom.choudhary015@gmail.com>
2026-03-21 14:23:31 +0530

    updated message in App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps!");
+        System.out.println("Hello from Hariom!");
     }
 
     public static int add(int a, int b) {
